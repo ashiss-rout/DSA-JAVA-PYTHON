39d67f868c06316cdcb15015c38bc532e8a13665
ashiss-rout <154452898+ashiss-rout@users.noreply.github.com>
2026-08-18 18:09:21 +0530

    first unique character in string

--- Hashmap/uni_char.py
+++ Hashmap/uni_char.py
@@ -1,1 +1,12 @@
+#first unique char in a string 
 
+def first_unique(s):
+    for ch in s:
+        if s.count(ch) == 1:
+            return ch
+
+    return None
+
+
+s = "leetcode"
+print("The first unique character is:", first_unique(s))
